c8ea654f1e971c0dfc907550d6209e23e2da59c0
laz <laz@pop-os.localdomain>
2021-03-18 19:23:13 +0100

    t-shirt

--- pages/bizarrebazar.js
+++ pages/bizarrebazar.js
@@ -62,7 +62,9 @@ export default function Home({ artistes = [] }) {
 
                 <div className="grid gap-6 grid-cols-3">
                   <div>
-                    {/* ////////oeuvre 1 */}
+                    {/*//////////////////////////////
+                     ////////oeuvre 1////////////////
+                    ////////////////////////////////// */}
 
                     {artiste.fields.illustrationOeuvre1 && (
                       <Carousel
@@ -95,15 +97,15 @@ export default function Home({ artistes = [] }) {
 
 
                     <button className="snipcart-add-item"
+                     data-item-id={artiste.fields.oeuvre1}
                       data-item-price={artiste.fields.prix1}
                       data-item-url="/bizarrebazar"
                     //  data-item-image={artiste.fields.illustrationOeuvre1[0]}
                       data-item-name={artiste.fields.oeuvre1}
-                      data-item-name={artiste.fields.oeuvre1}
                       data-item-custom1-name="Prix libre / Faire un don :"
                       data-item-custom1-options="0 euros [+0.00] | 2 euros[+2.00]|5 euros[+5.00]|10 euros[+10.00]|15 euros[+15.00]"
-                      data-item-custom2-name= "Taille" 
-                      data-item-custom2-options ="S|M|L|XL"
+                      // data-item-custom2-name= "Taille" 
+                      // data-item-custom2-options ="S|M|L|XL"
                       // data-item-shippable={artiste.fields.envoyer1}
                     >
                       Ajouter au panier
